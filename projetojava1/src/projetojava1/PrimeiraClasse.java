d3933113f3e362ef8ab66ad57a1d14ae4126fb7f
Janderson R. Pereira <janderprog@yahoo.com.br>
2025-07-11 17:00:39 -0300

    Primeira Classe

---
+++ projetojava1/src/projetojava1/PrimeiraClasse.java
@@ -0,0 +1,12 @@
+package projetojava1;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Primeiro programa em Java");
+
+	}
+
+}
